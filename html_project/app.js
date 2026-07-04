3923cb921adf8d757e448638b78fe0852205d5d1
Zeddic-SMO <samuel93ortil@gmail.com>
2026-07-04 20:32:50 +0100

    feat: html basic project

---
+++ html_project/app.js
@@ -0,0 +1,17 @@
+const container = document.querySelector(".container");
+
+console.log(container);
+
+const renderUI = () => {
+  container.innerHTML = `
+    <h1>Hello World</h1>
+    <button>Click me</button>
+    `;
+
+  const button = document.querySelector("button");
+  button.addEventListener("click", () => {
+    alert("Hi there!");
+  });
+};
+
+renderUI();
